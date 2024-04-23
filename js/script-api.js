0979ce13526b8b549c775ebe564d1c04545eff9f
khallaf <engayman.khalaf@gmail.com>
2024-04-23 12:19:16 +0200

    selected characters successfull

--- js/script-api.js
+++ js/script-api.js
@@ -5,11 +5,7 @@ async function waitingForResponse() {
       "https://akabab.github.io/superhero-api/api/all.json"
     );
     const characters = await response.json();
-
-    const names = getName(characters);
-    displayNames(names);
-    console.log(names)
-    //console.table(characters);
+    getNameAndImg(characters);
   } catch (error) {
     console.error("Unable to load character datas from the server : " + error);
   }
@@ -17,24 +13,61 @@ async function waitingForResponse() {
 
 waitingForResponse();
 
-function getName(characters) {
-let names = []
+function getNameAndImg(characters) {
+  let names = [];
+  let imgs = [];
   for (const c of characters) {
-    c.name;
-    names.push(c.name)
+    imgs.push(c.images.ms);
+    names.push(c.name);
+    const PlayerTemplate = document.getElementById("character-choice");
+    const player = document.importNode(PlayerTemplate.content, true);
+    const characters = document.getElementById("characters-list");
+    player.querySelector(".js-player-name").textContent = c.name;
+    player.querySelector(".js-player-img").alt = c.name;
+    player.querySelector(".js-player-img").src = c.images.md;
+    player.querySelector(".js-player-img").dataset.idImg = c.id;
+    player.querySelector(".js-player").dataset.id = c.id;
+
+    characters.appendChild(player);
   }
-  return names
-
 }
 
-function displayNames(names) {
-    for (const name of names) {
+// function ShowTemplates(
+//   injectedHtmlId,
+//   templeteID,
+//   templeteName,
+//   templeteSrc,
+//   templeteAlt,
+//   name,
+//   imgSrc
+// ) {
+//   const selectedCharacter = document.getElementById(injectedHtmlId);
 
-        const PlayerTemplate = document.getElementById("character-choice");
-        const player = document.importNode(PlayerTemplate.content, true);
-        player.querySelector(".js-player-name").textContent = name
-        document.body.appendChild(player);
-    }
-}
+//   const PlayerTemplate = document.getElementById(templeteID);
+//   const player = document.importNode(PlayerTemplate.content, true);
+//   player.querySelector(templeteName).textContent = name;
+//   player.querySelector(templeteAlt).alt = name;
+//   player.querySelector(templeteSrc).src = imgSrc;
 
+//   selectedCharacter.appendChild(player);
+// }
 
+const charactersLst = document.getElementById("characters-list");
+charactersLst.addEventListener("click", function (e) {
+  const PlayerTemplate = document.getElementById("selected-character");
+  const player = document.importNode(PlayerTemplate.content, true);
+  const selectedCharacter = document.getElementById("selected-characters-lst");
+  player.querySelector(".js-player-name").textContent = e.target.alt;
+  player.querySelector(".js-player-img").alt = e.target.alt;
+  player.querySelector(".js-player-img").src = e.target.src;
+  player.querySelector(".js-player-img").dataset.idImg = e.target.dataset.idImg;
+  player.querySelector(".js-player").dataset.id = e.target.dataset.idImg;
+
+  //   player.querySelector(".js-player").appendChild(e.target)
+  console.log(e.target);
+//   console.log(selectedCharacter);
+
+  selectedCharacter.appendChild(player);
+});
+
+function getClicked() {}
