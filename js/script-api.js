46ddf9e57068db2adb7eda0db3b1af21878a6a3a
khallaf <engayman.khalaf@gmail.com>
2024-04-23 14:45:46 +0200

    display gender

--- js/script-api.js
+++ js/script-api.js
@@ -1,3 +1,47 @@
+/**
+ * Display all characters available
+ * @param {object} characters
+ */
+function displayCharacters(characters) {
+  for (const c of characters) {
+    showTemplates("character-choice", "characters-list", c);
+  }
+}
+
+/**
+ * Show a template
+ * @param {string} templeteId - id of the template
+ * @param {String} injectedId - id of the injected element
+ * @param {object} object - an object with name, image, id
+ */
+function showTemplates(templeteId, injectedId, object) {
+  const playerTemplate = document.getElementById(templeteId);
+  const player = document.importNode(playerTemplate.content, true);
+  const charactersLst = document.getElementById(injectedId);
+  player.querySelector(".js-player-name").textContent = object.name;
+  player.querySelector(".js-player-img").alt = object.name;
+  player.querySelector(".js-player-img").src = object.images.md;
+  player.querySelector(".js-player-img").dataset.idImg = object.id;
+  player.querySelector(".js-player").dataset.id = object.id;
+  charactersLst.appendChild(player);
+}
+
+function showTemplatesFromHtml(templeteId, injectedId, object, characters) {
+  const PlayerTemplate = document.getElementById(templeteId);
+  const player = document.importNode(PlayerTemplate.content, true);
+  const selectedCharacter = document.getElementById(injectedId);
+  player.querySelector(".js-player-name").textContent = object.alt;
+  player.querySelector(".js-player-img").alt = object.alt;
+  player.querySelector(".js-player-img").src = object.src;
+  player.querySelector(".js-player-img").dataset.idImg = object.dataset.idImg;
+  player.querySelector(".js-player").dataset.id = object.dataset.idImg;
+  player.querySelector(".js-player-features").textContent =
+    characters[object.dataset.idImg].appearance.gender;
+  console.log(object.dataset.idImg);
+  selectedCharacter.appendChild(player);
+  return characters[object.dataset.idImg];
+}
+
 const allCharacters = [];
 async function waitingForResponse() {
   try {
@@ -5,69 +49,26 @@ async function waitingForResponse() {
       "https://akabab.github.io/superhero-api/api/all.json"
     );
     const characters = await response.json();
-    getNameAndImg(characters);
+    console.table(characters);
+    // console.log(characters[0].appearance.gender);
+    displayCharacters(characters);
+
+    const clickedCharactersList = [];
+
+    const charactersLst = document.getElementById("characters-list");
+    charactersLst.addEventListener("click", function (e) {
+      const clickedCharacter = showTemplatesFromHtml(
+        "selected-character",
+        "selected-characters-lst",
+        e.target,
+        characters
+      );
+
+      console.log(clickedCharacter);
+    });
   } catch (error) {
     console.error("Unable to load character datas from the server : " + error);
   }
 }
 
 waitingForResponse();
-
-function getNameAndImg(characters) {
-  let names = [];
-  let imgs = [];
-  for (const c of characters) {
-    imgs.push(c.images.ms);
-    names.push(c.name);
-    const PlayerTemplate = document.getElementById("character-choice");
-    const player = document.importNode(PlayerTemplate.content, true);
-    const characters = document.getElementById("characters-list");
-    player.querySelector(".js-player-name").textContent = c.name;
-    player.querySelector(".js-player-img").alt = c.name;
-    player.querySelector(".js-player-img").src = c.images.md;
-    player.querySelector(".js-player-img").dataset.idImg = c.id;
-    player.querySelector(".js-player").dataset.id = c.id;
-
-    characters.appendChild(player);
-  }
-}
-
-// function ShowTemplates(
-//   injectedHtmlId,
-//   templeteID,
-//   templeteName,
-//   templeteSrc,
-//   templeteAlt,
-//   name,
-//   imgSrc
-// ) {
-//   const selectedCharacter = document.getElementById(injectedHtmlId);
-
-//   const PlayerTemplate = document.getElementById(templeteID);
-//   const player = document.importNode(PlayerTemplate.content, true);
-//   player.querySelector(templeteName).textContent = name;
-//   player.querySelector(templeteAlt).alt = name;
-//   player.querySelector(templeteSrc).src = imgSrc;
-
-//   selectedCharacter.appendChild(player);
-// }
-
-const charactersLst = document.getElementById("characters-list");
-charactersLst.addEventListener("click", function (e) {
-  const PlayerTemplate = document.getElementById("selected-character");
-  const player = document.importNode(PlayerTemplate.content, true);
-  const selectedCharacter = document.getElementById("selected-characters-lst");
-  player.querySelector(".js-player-name").textContent = e.target.alt;
-  player.querySelector(".js-player-img").alt = e.target.alt;
-  player.querySelector(".js-player-img").src = e.target.src;
-  player.querySelector(".js-player-img").dataset.idImg = e.target.dataset.idImg;
-  player.querySelector(".js-player").dataset.id = e.target.dataset.idImg;
-
-  //   player.querySelector(".js-player").appendChild(e.target)
-  console.log(e.target);
-//   console.log(selectedCharacter);
-
-  selectedCharacter.appendChild(player);
-});
-
-function getClicked() {}
